c7b5755be01b963d58134962a01d3eb120e33982
Dan Fehrenbach <dnfehrenbach@gmail.com>
2014-07-29 10:59:24 -0500

    no more vim stuff - use vanilla vim for now

--- make_dot_links.sh
+++ make_dot_links.sh
@@ -11,18 +11,3 @@ fi
 ln -s ~/.dotfiles/cmd_line/tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/cmd_line/dir_colors ~/.dir_colors
 
-if ! [ -e ~/.vim ]
-then
-	ln -s ~/.dotfiles/vim_files/vim ~/.vim
-else
-	mv .vim .backup.vim
-	ln -s ~/.dotfiles/vim_files/vim ~/.vim
-fi
-
-if ! [ -e ~/.vimrc ]
-then
-	ln -s ~/.dotfiles/vim_files/vimrc ~/.vimrc
-else
-	mv .vimrc .backup.vimrc
-	ln -s ~/.dotfiles/vim_files/vimrc ~/.vimrc
-fi
