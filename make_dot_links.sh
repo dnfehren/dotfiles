12270f76a9e6d3cd84889527a6c63f5d711d3e48
Daniel Fehrenbach <dfehrenbach@cntenergy.org>
2015-05-18 09:38:15 -0500

    new wallpapers

--- make_dot_links.sh
+++ make_dot_links.sh
@@ -2,14 +2,14 @@
 
 if ! [ -e ~/.bashrc ]
 then
-	ln -s ~/.dotfiles/cmd_line/bashrc ~/.bashrc
+	ln -s ~/.dotfiles/bash/bashrc ~/.bashrc
 else
 	mv .bashrc .backup.bashrc
-	ln -s ~/.dotfiles/cmd_line/bashrc ~/.bashrc
+	ln -s ~/.dotfiles/bash/bashrc ~/.bashrc
 fi
 
-ln -s ~/.dotfiles/cmd_line/tmux.conf ~/.tmux.conf
-ln -s ~/.dotfiles/cmd_line/dir_colors ~/.dir_colors
+ln -s ~/.dotfiles/bash/tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/bash/dir_colors ~/.dir_colors
 
 if ! [ -e ~/.vim ]
 then
